304a30fd5dfffc1361ad69f87345f44bb370c294
neviko <nevosayag@gmail.com>
2022-12-20 17:14:38 +0200

    refactor - remove express from index.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signupRouter} from './routes/signup';
+import {errorHandler} from './middlewares/error-handler';
+import {NotFoundError} from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy',true); // we trust proxy because traffic will be arrived via ingress nginx
+app.use(json());
+app.use(cookieSession({
+  signed:false, // disables encryption
+  secure: true, // must use HTTPS
+
+}));
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(errorHandler);
+
+// if a route not found call to not found error, which it will call to error handler 
+app.all('*',async ()=>{
+  throw new NotFoundError();
+});
+
+
+export {app};
